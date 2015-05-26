25b5bdb845afad34dbb885b7b96cb41e413cc0b1
Tomasz Rebizant <trebizant@craftware.biz>
2015-05-26 15:44:51 +0200

    second commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright © 2015 by Craftware Sp. z o.o. All rights reserved.
+ *
+ * It is strictly prohibited to copy, distribute, publish or modify
+ * this file or any of its parts without the prior written consent of
+ * Craftware Sp. z o.o. (info@craftware.pl)
+ *
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("testowy projekt");
+	}
+}
